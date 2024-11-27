d2c74db21754420626be1aa159ed8b58ba23f67a
wuxianggujun <3344207732@qq.com>
2024-11-27 20:59:58 +0800

    先备份一下，即将通过xmake替换

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include "MainWindow.hpp"
 #include <QApplication>
+#include "include/core/SkBitmap.h"
+// Skia
+#include <include/core/SkCanvas.h>
+#include <include/core/SkBitmap.h>
+#include <include/core/SkPaint.h>
+#include <include/core/SkTypeface.h>
+#include <include/core/SkFont.h>
+#include <include/codec/SkCodec.h>
+
 
 int main(int argc, char *argv[])
 {
+
+    SkBitmap bitmap;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
