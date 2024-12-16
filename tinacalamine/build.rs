37a5efaf844f1d73d712afc9f8f22e9fb5021a94
wuxianggujun <3344207732@qq.com>
2024-12-16 15:09:20 +0800

    删除无用依赖和项目，添加Rust库。

--- tinacalamine/build.rs
+++ tinacalamine/build.rs
@@ -1,10 +1,5 @@
+#[warn(unused_must_use)]
 fn main() {
-    cxx_build::bridge("src/main.rs")
-        .file("src/blobstore.cc")
-        .std("c++17")
-        .compile("tinacalamine");
-
-    println!("cargo:rerun-if-changed=src/main.rs");
-    println!("cargo:rerun-if-changed=src/blobstore.cc");
-    println!("cargo:rerun-if-changed=include/blobstore.h");
+    cxx_build::bridge("src/lib.rs");
+    println!("cargo:rerun-if-changed=src/lib.rs");
 }
